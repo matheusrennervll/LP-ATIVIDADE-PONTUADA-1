daa5715f818eeb0396d9c856f6bb96e30a228166
renner.vll <matheusttk79@gmail.com>
2026-03-11 13:20:48 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,6 +1,29 @@
 import os
 os.system('cls')
 
-num1 = int(input('Digite o primeiro número:'))
-operador = input('Digite a operação desejada ( + - * / )')
-num2 = int(input('Digite o segundo número'))
+operacao = input("Digite a operação desejada (+, -, *, /): ")
+A = int(input("Digite o valor do número inteiro A: "))
+B = int(input("Digite o valor do número inteiro B: "))
+
+if operacao == '+':
+    resultado = A + B
+    print(f"\nResultado: {A} {operacao} {B} = {resultado}")
+
+elif operacao == '-':
+    resultado = A - B
+    print(f"\nResultado: {A} {operacao} {B} = {resultado}")
+
+elif operacao == '*':
+    resultado = A * B
+    print(f"\nResultado: {A} {operacao} {B} = {resultado}")
+
+elif operacao == '/':
+
+    if B == 0:
+        print("\nErro: Não é possível dividir um número por zero.")
+    else:
+        resultado = A / B
+        print(f"\nResultado: {A} {operacao} {B} = {resultado}")
+
+else:
+    print("\nErro: Operação inválida. Por favor, digite +, -, * ou /.")
